6a57294f15b05783bcf480cb5976ada5d8cad61b
iam-benyamin <benyaminmahmoudyan@gmail.com>
2023-01-24 01:01:16 +0330

    fix/header-on-scroll-change-style

--- assets/index.js
+++ assets/index.js
@@ -4,19 +4,18 @@ const darkBlueColor = getComputedStyle(document.body).getPropertyValue('--dark-b
 const blackColor = getComputedStyle(document.body).getPropertyValue('--black');
 const logo = document.querySelector('#header .logo img');
 const headerATags = document.querySelectorAll('#header a');
-
-const ATags = document.querySelector('#header a');
-const ATagsAfter = window.getComputedStyle(ATags, '::after');
-console.log(ATagsAfter)
+let styleHeadElement = document.head.appendChild(document.createElement('style'));
 
 document.addEventListener('scroll', () => {
     if (window.scrollY >= 30) {
         logo.setAttribute('src', 'assets/haneh_logo_light.png');
         header.style.backgroundColor = whiteColor;
         headerATags.forEach(e => e.style.color = blackColor);
+        styleHeadElement.innerHTML = '#header .nav li a::after {background-color: #2c303d;bottom: 0}';
     } else {
         logo.setAttribute('src', 'assets/haneh_logo_dark.png');
         header.style.backgroundColor = darkBlueColor;
         headerATags.forEach(e => e.style.color = whiteColor);
-    }   
+        styleHeadElement.innerHTML = '#header .nav li a::after {background-color: #ffffff;bottom: -3px;}';
+    }
 });
